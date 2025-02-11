5c485992b808c99c0c6a90380184f5159b481303
Sreejith1110 <sreejithmanoj1110@gmail.com>
2025-02-11 23:47:59 +0530

    Changed storing data from json to mysql

--- task08/backend.py
+++ task08/backend.py
@@ -1,64 +1,80 @@
 from flask import Flask,jsonify,request;
+from flask_sqlalchemy import SQLAlchemy;
 from flask_cors import CORS;
-import json;
 
 app = Flask(__name__)
 CORS(app)
 
-def read_data():
-    with open('./database.json','r') as f:
-        return(json.load(f))
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:newpassword@127.0.0.1/mydb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
-def write_data(data):
-    with open('./database.json','w') as f:
-        json.dump(data,f,indent=4)
+db=SQLAlchemy(app)
+
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username =db.Column(db.String(50),unique=True, nullable=False)
+    password= db.Column(db.String(200),nullable=False)
+
+class Watchlater(db.Model):
+    username = db.Column(db.String(50),unique=True,nullable=False)
+    Movie_id=db.Column(db.Integer,primary_key=True)
+    Movie_name=db.Column(db.String(300),nullable=False)
+    Movie_img = db.Column(db.String(500),nullable=False)
+    Year=db.Column(db.Integer,nullable=False)
+    Movie_description=db.Column(db.String(3000),nullable=False)
+    Movie_author=db.Column(db.String(100),nullable=False)
+
+class Review(db.Model):
+    Movie_id = db.Column(db.Integer,primary_key=True)
+    Review_id=db.Column(db.Integer,primary_key=True)
+    Review=db.Column(db.String(),nullable=True)
+
+with app.app_context():
+    db.create_all()
+@app.route('/api/review',methods=['GET'])
+def review_data():
+    comment=Review.query.all()
+    return(jsonify([{"Movie_id":m.Movie_id,"Review_id":m.Review_id,"Review_data":m.Review} for m in comment]))
+
+@app.route('/api/data', methods = ['GET'])
+def movie_data():
+    data=Watchlater.query.all()
+    return(jsonify([{"Username":m.username,"movie_id":m.Movie_id, "movie_name":m.Movie_name,"Movie_img":m.Movie_img, "year":m.Year,"movie_description":m.Movie_description,"movie_author":m.Movie_author} for m in data]))
+
+@app.route('/api/users', methods = ['GET'])
+def user_data():
+    users=Login.query.all()
+    return(jsonify([{"id":m.id,"username":m.username,"password":m.password} for m in users]))
+
+# @app.route('/api/moviedata', methods = ['GET'])
+# def movie_info():
+#     movieDataApi =''
 
 @app.route('/api/login', methods = ['POST'])
 def get_users():
-    data= read_data()
     userdata = request.get_json()
     username = userdata.get("username")
     password = userdata.get("password")
-    accessDenied = False
-    if not username or not password:
-        return jsonify({"message":"username and password are required"})
-    for info in data['user']:
-        if info['username'] == username:
-            if info['password'] == password:
-                return(jsonify({"message":" login successfully","data":info}))
-            else:
-                return(jsonify({"message":"Invalid Credentials"}))
-        else:
-            accessDenied = True
-            
-    if (accessDenied):
-        return(jsonify({"message":"Username does not exist"}))
 
-        
-@app.route('/api/data',methods=['GET'])
-def read_user():
-    data=read_data()
-    return(jsonify(data))
+    user = Login.query.filter_by(username=username).first()
+    if user and user.password == password:
+        return(jsonify({"message":"Login successful"}))
+    else:
+        return(jsonify({"message":"Invalid Credentials"}))
+
 
 @app.route('/api/register', methods = ['POST'])
 def add_user():
-    userdata=read_data()
     data = request.get_json()
     username=data.get("username")
     password=data.get("password")
-    if not username or not password:
-        return jsonify({"message":"username and password are required"})
-    if any(user['username'] == data['username'] for user in userdata['user']):
-        return jsonify({"message":"username already exists"})
-    userdata['user'].append({
-        "username":username,
-        "password":password,
-        "watchlist":[],
-        "Profile":{},
-        "recommendation":[],
-        "History":[]
-    })
-    write_data(userdata)
-    return(jsonify({"message":"successfully registered"}))
+    if Login.query.filter_by(username=username).first():
+        return(jsonify({"message":"username already exist"}))
+    else:
+        new_user=Login(username=username,password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return(jsonify({"message":"successfully registered"}))
+    
 if __name__=='__main__':
     app.run(debug=True)
